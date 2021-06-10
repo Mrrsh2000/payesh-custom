45c701a08988f304b600157bfd460c1fad12ad1d
mohammad shekari <6424019-mohammadshekari@users.noreply.gitlab.com>
2021-06-10 23:04:48 +0430

    Server Static Mode

--- payesh/settings.py
+++ payesh/settings.py
@@ -143,17 +143,24 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#
+# MEDIA_URL = '/media/'
+
+# Server Mode:
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [BASE_DIR + "/static", ]
+STATIC_ROOT = '/home/mahangol/public_html/payesh/static'
+MEDIA_ROOT = '/home/mahangol/public_html/payesh/media'
 
 JWT_AUTH = {
     'JWT_ENCODE_HANDLER':
